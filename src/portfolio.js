3d51a39ed40bc9c000aa269b606b844d1fbfe9b6
mahendra <conquer@Conquers-MacBook-Pro.local>
2021-05-13 13:47:25 +0530

    portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -37,6 +37,12 @@ const socialMediaLinks = [
     backgroundColor: "#0077B5", 
   },
   {
+    name: "YouTube",
+    link: "https://www.youtube.com/channel/UCIf7QLKvNYQIFCRxCe-j8dQ",
+    fontAwesomeIcon: "fa-youtube", // Reference https://fontawesome.com/icons/youtube?style=brands
+    backgroundColor: "#FF0000", // Reference https://simpleicons.org/?q=youtube
+  },
+  {
     name: "Gmail",
     link: "mailto:mahendragandham730@gmail.com",
     fontAwesomeIcon: "fa-google", 
